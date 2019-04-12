424074392a44ba116627bf091c0835a8819fc353
Edwin Olivera <39258306+EdwinOlivera@users.noreply.github.com>
2019-04-11 23:11:37 -0600

    Creacion del Proyecto

---
+++ Simulador_De_Banco/src/simulador_de_banco/Simulador_De_Banco.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simulador_de_banco;
+
+/**
+ *
+ * @author Edwin
+ */
+public class Simulador_De_Banco {
+public static String MensajeFinal="*****************<<<<<<Este es el final del Programa>>>>>*****************";
+    public static void main(String[] args) {
+        System.out.println(MensajeFinal);
+    }
+    
+}
