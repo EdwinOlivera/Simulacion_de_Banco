e1197ae5ddfad2f9c401106a1430f54ec8764a69
Edwin Olivera <39258306+EdwinOlivera@users.noreply.github.com>
2019-04-17 13:24:33 -0600

    Creacion del Formulario
    Se ha establecido el primer diseño del formulario y como sera su funcionamiento

--- Simulador_De_Banco/src/simulador_de_banco/Simulador_De_Banco.java
+++ Simulador_De_Banco/src/simulador_de_banco/Simulador_De_Banco.java
@@ -5,13 +5,18 @@
  */
 package simulador_de_banco;
 
+import Fomularios.SalaDeEspera;
+
 /**
  *
  * @author Edwin
  */
 public class Simulador_De_Banco {
+        private static  SalaDeEspera Formulario = new SalaDeEspera();
 public static String MensajeFinal="*****************<<<<<<Este es el final del Programa>>>>>*****************";
     public static void main(String[] args) {
+
+    Formulario.setVisible(true);
         System.out.println(MensajeFinal);
     }
     
